f527347e925c85977bc90b716b86b80c99f65041
Jktfe <j.w.p.king@gmail.com>
2026-05-21 22:06:49 +0100

    fix antchat compatibility formula source

--- Formula/antchat.rb
+++ Formula/antchat.rb
@@ -1,7 +1,9 @@
 class Antchat < Formula
   desc "Compatibility command for the Antchat Mac app"
   homepage "https://www.antonline.dev/"
+  url "https://raw.githubusercontent.com/Jktfe/homebrew-antchat/bb9498e/README.md"
   version "0.1.8"
+  sha256 "acf2e285ebf1b095bc0042b9f60cb26ac5c0891cf957ea0c60731aa70fb1573f"
   license "AGPL-3.0-or-later"
 
   depends_on "jktfe/antchat/ant"
